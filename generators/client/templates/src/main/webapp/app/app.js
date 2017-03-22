3863b104e8c750c04434a5e7da13920c36eeab48
SendilKumar N <sendilkumarn@live.com>
2017-03-22 16:52:46 +0800

    Added fixed data table component

--- generators/client/templates/src/main/webapp/app/app.js
+++ generators/client/templates/src/main/webapp/app/app.js
@@ -1,4 +1,5 @@
 import 'flexboxgrid/dist/flexboxgrid.css';
+import 'fixed-data-table-2/dist/fixed-data-table.css';
 import React, { Component, PropTypes } from 'react';
 import { connect } from 'react-redux';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
